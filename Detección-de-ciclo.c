5770a2cd123542bfc2f96a7c3ed75a469b8bcc2e
Pacorrote <42156807+Pacorrote@users.noreply.github.com>
2018-09-02 03:09:19 -0500

    Create Detección-de-ciclo.c

---
+++ Detección-de-ciclo.c
@@ -0,0 +1,18 @@
+bool has_cycle(SinglyLinkedListNode* head) {
+   // Complete this function
+    // Do not write the main method
+    if(head == NULL)    return false;
+    
+    SinglyLinkedListNode* slow = head;
+    SinglyLinkedListNode* fast = head->next;
+    
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(fast == slow)    return true;
+    }
+    
+    return false;
+}
